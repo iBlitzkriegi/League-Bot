f37845c41a8995d9d8b644eab4313d39dc3c7304
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-06-10 18:29:27 -0400

    Add config file reading

--- LeagueBot.py
+++ LeagueBot.py
@@ -1,10 +1,34 @@
 from discord.ext import commands
+from discord import Game
 
-bot = commands.Bot(command_prefix='!', description='Basic personal league of legends bot')
+name = None
+prefix = None
+description = None
+token = None
+
+for s in open('./ignore/config.yml'):
+    line = s.split('=')
+    key = line[0]
+    value = line[1].replace('\'', '').strip()
+    if 'name' in key:
+        name = value
+    elif 'prefix' in key:
+        prefix = value
+    elif 'desc' in key:
+        description = value
+    elif 'token' in key:
+        token = value
+
+bot = commands.Bot(command_prefix=prefix, description=description, activity=Game(name='Neeko is best decision'))
 
 
 @bot.event
 async def on_ready():
     print(bot.user.name + ' is now online!')
 
-bot.run('')
+
+if token is not None:
+    bot.run(token)
+    token = None
+else:
+    print('Error! Token wasn\'t parsed correctly! Was config corrupted?')
